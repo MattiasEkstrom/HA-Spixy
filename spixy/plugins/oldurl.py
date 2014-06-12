a94f96351a2c975ea011ccf1061de785484245fe
Mattias Ekström <mattias.ekstrom@gmail.com>
2014-06-12 23:50:06 +0200

    fix indent

--- spixy/plugins/oldurl.py
+++ spixy/plugins/oldurl.py
@@ -9,8 +9,8 @@ class OldUrlPlugin(Plugin):
     def __init__(self, config, client):
         client.register_listener("PRIVMSG", self._handle_url)
         self._client = client
-		self._store = {url : {}, score : {}} 
-		self._regex = re.compile(
+        self._store = {url : {}, score : {}} 
+        self._regex = re.compile(
             r'^((?:http|ftp)s?://)?' # http:// or https://
             r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
             r'localhost|' #localhost...
@@ -20,28 +20,28 @@ class OldUrlPlugin(Plugin):
 
         super(OldUrlPlugin, self).__init__(config)
 
-		# test data
-		self._store["url"]["www.apa.se"] = [("mts", datetime.now()), ("bjz", datetime.now())]
+        # test data
+        self._store["url"]["www.apa.se"] = [("mts", datetime.now()), ("bjz", datetime.now())]
 
     def _handle_url(self, nick, target, message, **rest):
         if not target.startswith("#"):
-			return 
+            return 
 
-		parts = message.split(" ")
-		for p in parts:
-			m = self._regex.match(p)
-		    
-			if m is None: continue
+        parts = message.split(" ")
+        for p in parts:
+            m = self._regex.match(p)
+            
+            if m is None: continue
 
-			print("bajs")
+            print("bajs")
 
-			if p not in self._store["url"]:
-				self._store["url"][p] = (nick, datetime.now())
-				print("added url: " + p)
-			else
-				print("this url is old: " + p)
-				
-		        #self.send_command(nick=nick, target=target, message=message)
+            if p not in self._store["url"]:
+                self._store["url"][p] = (nick, datetime.now())
+                print("added url: " + p)
+            else
+                print("this url is old: " + p)
+                
+                #self.send_command(nick=nick, target=target, message=message)
 
     def _handle_command(self, command):
         """choices = self._partition(command['message'])
@@ -57,5 +57,5 @@ class OldUrlPlugin(Plugin):
         else:
             self._client.privmsg(target=command['nick'], message=answer)"""
 
-		pass
+        pass
 
